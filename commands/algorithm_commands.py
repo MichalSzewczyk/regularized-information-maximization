8648646ba46aa4fcffb7a7d35fa714661903f5ff
mszewczyk <szewczykm94@gmail.com>
2018-01-19 13:53:45 +0100

    implemented minimization function

--- commands/algorithm_commands.py
+++ commands/algorithm_commands.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+from scipy import optimize
 from sklearn.cluster import KMeans
 
 import numpy as np
@@ -16,8 +17,8 @@ class NormalizingCommand(Command):
 
     def execute(self, input_data):
         self.logger.info('Input data for algorithm: \n{}'.format(input_data.X))
-        means_by_rows = np.mean(input_data.X, axis=1)
-        std_by_rows = np.std(input_data.X, axis=1)
+        means_by_rows = np.mean(input_data.X, axis=0)
+        std_by_rows = np.std(input_data.X, axis=0)
         input_data.X = (input_data.X - means_by_rows) / std_by_rows
         self.logger.info('Data after normalization: \n{}'.format(input_data.X))
 
@@ -34,3 +35,32 @@ class KMeansCommand(Command):
         self.logger.info('Labels of clusters: \n{}'.format(result.labels_))
 
         return result
+
+
+class GradientCommand(Command):
+    def __init__(self, logger):
+        self.logger = logger
+
+    def execute(self, input_data):
+        self.logger.info('Normalized input data: \n{}'.format(input_data.X))
+        result = optimize.minimize(fun, input_data.X, method="L-BFGS-B")
+        self.logger.info('Labels of clusters: \n{}'.format(result.labels_))
+
+        return result
+
+
+class FunctionCommand(Command):
+    def __init__(self, logger):
+        self.logger = logger
+
+    def execute(self, input_data):
+        self.logger.info('Normalized input data: \n{}'.format(input_data.X))
+
+
+        self.logger.info('Labels of clusters: \n{}'.format(result.labels_))
+
+        return result
+
+
+def fun(x, y):
+    return x ** 2 + y ** 2
--- data/data_supplier.py
+++ data/data_supplier.py
@@ -2,10 +2,20 @@ import numpy as np
 
 
 class InputDataSupplier:
+    def getData(self, path, sep, name=False):
+        with open(path) as file:
+            tmp = [line.strip().split(sep) for line in file]
+            data = []
+        if name:
+            for line in tmp:
+                data.append(list(filter(lambda x: len(x), line))[1:-1])
+        else:
+            for line in tmp:
+                data.append(list(filter(lambda x: len(x), line))[:-1])
+        return data
+
     def supply(self):
-        return AlgorithmData(np.array([[1, 11, 3],
-                         [1, 22, 3],
-                         [1, 33, 3], ]), 2)
+        return AlgorithmData(np.matrix(np.array(self.getData('data/lenses.txt', ' ', name=True))).astype(np.float), 3)
 
 
 class AlgorithmData:
